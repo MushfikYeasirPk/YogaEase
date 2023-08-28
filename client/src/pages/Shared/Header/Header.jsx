d832e38e1d455138004497366d9eea77e38a0e7d
Mushfik Yeasir <pkm2243@gmail.com>
2023-08-28 09:57:05 +0600

    change project title on header

--- client/src/pages/Shared/Header/Header.jsx
+++ client/src/pages/Shared/Header/Header.jsx
@@ -51,89 +51,6 @@ const Header = () => {
 
         <div>
 
-            {/* <div className="navbar bg-lime-400 px-5">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><NavLink to='/' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}>Home</NavLink></li>
-                            <li><NavLink to='/instructors' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}>Instructors</NavLink></li>
-                            <li><NavLink to='/classes' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}>Classes</NavLink></li>
-
-                            {
-                                user ?
-                                    <>
-
-                                        <li className='cursor-pointer'><NavLink to='/dashboard/mycart' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}><FaShoppingCart></FaShoppingCart>Dashboard +{ClassCart?.length || 0}</NavLink>
-
-                                        </li>
-
-
-
-
-                                    </>
-                                    : <></>
-                            }
-
-                        </ul>
-                    </div>
-                    <a className="text-xl">Yogastic.</a>
-
-
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><NavLink to='/' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}>Home</NavLink></li>
-                        <li><NavLink to='/instructors' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}>Instructors</NavLink></li>
-                        <li><NavLink to='/classes' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}>Classes</NavLink></li>
-
-
-                    </ul>
-
-                </div>
-
-                <div >
-                    {
-                        user ?
-                            <>
-                                <div className="navbar-center hidden lg:flex">
-                                    <ul className="menu menu-horizontal px-1">
-
-                                        <li className='cursor-pointer'><NavLink to='/dashboard/mycart' className={({ isActive }) => (isActive ? 'text-blue-700' : 'default')}><FaShoppingCart></FaShoppingCart>Dashboard +{ClassCart?.length || 0}</NavLink>
-
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div className="dropdown dropdown-end navbar-end">
-                                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                                        <div onMouseOver={onMouseOver} className="w-10 rounded-full">
-                                            <img src={user?.photoURL} title={userName} />
-
-                                        </div>
-                                    </label>
-                                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-
-                                        <li><Link onClick={handleLogout}>Logout</Link></li>
-                                    </ul>
-                                </div>
-                            </> :
-                            <>
-                                <Link to='/signIn'><button className="btn btn-accent btn-success">Sign in</button></Link>
-                            </>
-                    }
-
-
-
-                </div>
-            <div>
-                <button>ahs</button>
-            </div>
-            </div> */}
-
-
             <div className="navbar bg-base-100">
                 <div className="flex-none">
                     <div className="dropdown">
@@ -164,7 +81,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">Yogastic.io</a>
+                    <a className=" text-xl">YogaEase</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
