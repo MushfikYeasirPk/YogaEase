4d167dd6e48081d5ff6a82a5167d8fb77cc12f38
Mushfik Yeasir <pkm2243@gmail.com>
2023-08-28 11:11:44 +0600

    remove react responsive carousel and use swiper

---
+++ client/src/Components/HomeCaousel/HomeCausel.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+import { Link } from 'react-router-dom';
+
+function HomeCausel() {
+
+    return (
+        <>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+
+                    <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(https://cdn.pixabay.com/photo/2019/06/26/09/52/girl-4300034_960_720.jpg)' }}>
+                        <div className="text-center text-white">
+                            <h1 className="text-3xl font-bold mb-4">MAKE YOUR BODY & SOUL <br></br>
+                                FIT & WELL</h1>
+                            <Link to="/contact" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Join Classes Now</span>
+                            </Link>
+                        </div>
+                    </div>
+
+
+                </SwiperSlide>
+                <SwiperSlide>
+
+                <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(https://cdn.pixabay.com/photo/2017/03/26/21/54/yoga-2176668_960_720.jpg)' }}>
+                        <div className="text-center text-white">
+                            <h1 className="text-3xl font-bold mb-4">MAKE YOUR BODY & SOUL <br></br>
+                                FIT & WELL</h1>
+                            <Link to="/contact" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Join Classes Now</span>
+                            </Link>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+
+                <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(https://cdn.pixabay.com/photo/2020/03/29/18/33/girl-4981766_960_720.jpg)' }}>
+                        <div className="text-center text-white">
+                            <h1 className="text-3xl font-bold mb-4">MAKE YOUR BODY & SOUL <br></br>
+                                FIT & WELL</h1>
+                            <Link to="/contact" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Join Classes Now</span>
+                            </Link>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+        </>
+    )
+}
+
+export default HomeCausel
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import About from '../AboutUS/About';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+
 import PopularInstructors from '../Popular Instructors/PopularInstructors';
 import PopularClasses from '../PopularClasses/PopularClasses';
+import HomeCausel from '../../Components/HomeCaousel/HomeCausel';
 
 
 
@@ -12,30 +12,7 @@ const Home = () => {
 
   return (
     <div>
-      <Carousel>
-        <div className=''>
-            <img src="https://img.freepik.com/free-photo/silhouette-woman-doing-yoga-beach_1286-47.jpg?1&w=740&t=st=1686236003~exp=1686236603~hmac=66079d0a9d838ca5346bcf6164294e7e17a566ae8fea9d120e07db4621904f8f" />
-          <div>
-          <p className='legend text-4xl'>Flexibility is a second power</p>
-          </div>
-          
-
-        </div>
-        <div>
-          <img src="https://img.freepik.com/free-photo/attractive-girl-with-long-black-hair-dressed-purple-doing-yoga-pose-one-leg-pigeon_1268-20287.jpg?w=740&t=st=1686236157~exp=1686236757~hmac=bb84c7b7749dc0fc85ebc3def63daed3245e0ea5bca10e764508fc42d5822df7" />
-          <div>
-          <p className='legend text-4xl'>Yoga is way to freedom</p>
-          </div>
-
-        </div>
-        <div>
-          <img src="https://img.freepik.com/free-photo/young-man-yoga-pose_23-2147645674.jpg?w=740&t=st=1686236125~exp=1686236725~hmac=b13b75f6e4bef09764ed3815f6bda45611206502d96be288e8ce651af3e716c9" />
-          <div>
-          <p className='legend text-4xl'>Yoga ans meditation for busy people</p>
-          </div>
-        </div>
-    
-      </Carousel>
+    <HomeCausel></HomeCausel>
       <PopularClasses></PopularClasses>
       <PopularInstructors></PopularInstructors>
       <About></About>
