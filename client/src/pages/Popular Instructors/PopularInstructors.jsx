9af8e1eef7149becfcf75080720c5183e89408c0
Mushfik Yeasir <pkm2243@gmail.com>
2023-08-29 13:36:19 +0600

    solve issue in instructor image

--- client/src/pages/Popular Instructors/PopularInstructors.jsx
+++ client/src/pages/Popular Instructors/PopularInstructors.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { Fade } from 'react-awesome-reveal';
 import UseInstructors from '../Custom Hooks/UseInstructors';
 const PopularInstructors = () => {
     const [instructors] = UseInstructors();
 
     return (
         <div>
-            <Fade right big>
+            <div >
                 <h1 className='py-5 text-center text-4xl text-cyan-700 border-b-4 border-indigo-500'>Popular Instructors</h1>
-            </Fade>
+            </div>
             
-            <div className='grid gap-6 md:grid-cols-3 sm:grid-cols-2 mx-16 mt-5'>
+            <div className='grid gap-6 lg:grid-cols-3 sm:grid-cols-2 mx-16 mt-5'>
                 {
                     instructors.slice(0, 6).map((sigleInstructors) =>
-                        <Fade bottom big className="card w-96 bg-base-100 shadow-xl" key={sigleInstructors._id}>
+                        <div   key={sigleInstructors._id}>
                             <figure><img src={sigleInstructors.image} alt="Shoes" /></figure>
 
-                        </Fade>
+                        </div>
                     )}
 
             </div>
