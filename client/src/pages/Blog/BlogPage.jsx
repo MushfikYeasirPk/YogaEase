dcd20c95cbe45119806447ec0db97817a087441e
Mushfik Yeasir <pkm2243@gmail.com>
2023-08-31 02:15:05 +0600

    blog page right side done

--- client/src/pages/Blog/BlogPage.jsx
+++ client/src/pages/Blog/BlogPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function BlogPage() {
   return (
     <>
-      <div className='grid lg:grid-cols-3 gap-4 m-8 '>
+      <div className='grid lg:grid-cols-3 gap-3 m-16 '>
         <div className='col-span-2'>
 
           {blog.map((singleBlog, index) => (
@@ -25,7 +25,174 @@ function BlogPage() {
           ))}
         </div>
         <div>
-          <p>hi</p>
+          <form className="flex">
+            <input type="text" placeholder="Search Blog" className="input input-bordered input-success w-full max-w-xs" />
+          </form>
+          <div className="flex flex-col bg-[#f8f8f8] my-[35px] p-[30px]">
+              <p className="text-[18px] text-black font-bold mb-5">
+                Categories
+              </p>
+              <span className="w-[40px] h-[3.5px] bg-purple-800"></span>
+              <ul className="text-[16px] text-[#7e7e7e] font-medium mt-10">
+                <li className="cursor-pointer flex justify-between border-b border-[#dcd9d9] pb-6 mb-10 hover:text-purple-800 ease-in duration-200">
+                  <p>
+                  <span className='text-xl'>&#8594;</span>  Body Building
+                  </p>
+                  <span>(4)</span>
+                </li>
+
+                <li className="cursor-pointer flex justify-between border-b border-[#dcd9d9] pb-6 mb-10 hover:text-purple-800 ease-in duration-200">
+                  <p>
+                  
+                  <span className='text-xl'>&#8594;</span> Boxing
+                  </p>
+                  <span>(4)</span>
+                </li>
+
+                <li className="cursor-pointer flex justify-between border-b border-[#dcd9d9] pb-6 mb-10 hover:text-purple-800 ease-in duration-200">
+                  <p>
+                  <span className='text-xl'>&#8594;</span> Crossfit
+                  </p>
+                  <span>(4)</span>
+                </li>
+
+                <li className="cursor-pointer flex justify-between border-b border-[#dcd9d9] pb-6 mb-10 hover:text-purple-800 ease-in duration-200">
+                  <p>
+                  <span className='text-xl'>&#8594;</span> Fitness
+                  </p>
+                  <span>(4)</span>
+                </li>
+
+                <li className="flex justify-between border-b border-[#dcd9d9] pb-6 mb-10 hover:text-purple-800 ease-in duration-200">
+                  <p>
+                  <span className='text-xl'>&#8594;</span> Meditation
+                  </p>
+                  <span>(4)</span>
+                </li>
+
+                <li className="cursor-pointer flex justify-between border-b border-[#dcd9d9] pb-6 mb-10 hover:text-purple-800 ease-in duration-200">
+                  <p>
+                  <span className='text-xl'>&#8594;</span> Yoga
+                  </p>
+                  <span>(4)</span>
+                </li>
+              </ul>
+            </div>
+            {/* recent posts */}
+            <div className="flex flex-col bg-[#f8f8f8] my-[35px] p-[30px]">
+              <p className="text-[18px] text-black font-bold mb-5">
+                Recent Posts
+              </p>
+              <span className="w-[40px] h-[3.5px] bg-[#ff0336] mb-7"></span>
+              <div className="flex flex-col gap-7">
+                {/*  */}
+                <div className="flex gap-8">
+                  <img src={'https://images.pexels.com/photos/6932883/pexels-photo-6932883.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1'} alt="recent_img" className="w-[6rem]" />
+                  <div className="flex flex-col gap-4 justify-center">
+                    <p className="text-[14px] text-[#646464] font-medium">
+                      March 22, 2022
+                    </p>
+                    <p className="text-[16px] text-black hover:text-purple-800 ease-in duration-200 cursor-pointer font-bold">
+                      Yoga For Everyone in 2022
+                    </p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex gap-8">
+                  <img src={'https://images.pexels.com/photos/6698513/pexels-photo-6698513.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="recent_img" className="w-[6rem]" />
+                  <div className="flex flex-col gap-4 justify-center">
+                    <p className="text-[14px] text-[#646464] font-medium">
+                      March 22, 2022
+                    </p>
+                    <p className="text-[16px] text-black hover:text-purple-800 ease-in duration-200 cursor-pointer font-bold">
+                      Getting Back Into CrossFit
+                    </p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex gap-8">
+                  <img src={'https://images.pexels.com/photos/3286006/pexels-photo-3286006.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="recent_img" className="w-[6rem]" />
+                  <div className="flex flex-col gap-4 justify-center">
+                    <p className="text-[14px] text-[#646464] font-medium">
+                      March 22, 2022
+                    </p>
+                    <p className="text-[16px] text-black hover:text-purple-800 ease-in duration-200 cursor-pointer font-bold">
+                      Meet Fitness Abassador Grace
+                    </p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex gap-8">
+                  <img src={'https://images.pexels.com/photos/8172941/pexels-photo-8172941.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="recent_img" className="w-[6rem]" />
+                  <div className="flex flex-col gap-4 justify-center">
+                    <p className="text-[14px] text-[#646464] font-medium">
+                      March 22, 2022
+                    </p>
+                    <p className="text-[16px] text-black hover:text-purple-800 ease-in duration-200 cursor-pointer font-bold">
+                      The best are European Meditation Practitioner
+                    </p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex gap-8">
+                  <img src={'https://images.pexels.com/photos/8613312/pexels-photo-8613312.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="recent_img" className="w-[6rem]" />
+                  <div className="flex flex-col gap-4 justify-center">
+                    <p className="text-[14px] text-[#646464] font-medium">
+                      March 22, 2022
+                    </p>
+                    <p className="text-[16px] text-black hover:text-purple-800 ease-in duration-200 cursor-pointer font-bold">
+                      Learn Boxing With Our Trainer John
+                    </p>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="flex gap-8">
+                  <img src={'https://images.pexels.com/photos/1472887/pexels-photo-1472887.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="recent_img" className="w-[6rem]" />
+                  <div className="flex flex-col gap-4 justify-center">
+                    <p className="text-[14px] text-[#646464] font-medium">
+                      March 22, 2022
+                    </p>
+                    <p className="text-[16px] text-black hover:text-purple-800 ease-in duration-200 cursor-pointer font-bold">
+                      How To Get Lean For The Summer
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+              {/* popular tags */}
+              <div className="flex flex-col bg-[#f8f8f8] my-[35px] p-[30px]">
+              <p className="text-[18px] text-black font-bold mb-5">
+                Popular Tags
+              </p>
+              <span className="w-[40px] h-[3.5px] bg-purple-800 mb-7"></span>
+              <div className="flex gap-3 text-[16px] text-[#646464] font-medium flex-wrap">
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Crossfit
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Fitness
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Gym
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Meditation
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Running
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Workout
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Yoga
+                </p>
+                <p className="bg-white py-[4px] px-[14px] hover:text-purple-800 ease-in duration-200 cursor-pointer">
+                  #Boxing
+                </p>
+              </div>
+            </div>
         </div>
 
       </div>
