269c6aa486c08992ff3d45ab66d68a2b8cfd7e6d
Mushfik Yeasir <pkm2243@gmail.com>
2023-08-30 10:16:59 +0600

    added background image and made responsive

--- client/src/Components/About/AboutCarts.jsx
+++ client/src/Components/About/AboutCarts.jsx
@@ -3,12 +3,15 @@ import { cardList } from "./cardList";
 function AboutCarts() {
   return (
     <>
-    <div className="mx-10 grid grid-cols-3 gap-6">
+    <div className="mx-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
 
       {cardList.map((card, id) => (
         <div
           key={id}
-          className="flex flex-col cursor-pointer bg-white justify-center py-6 px-10 text-center items-center mt-12 rounded-tl-[35px] rounded-br-[35px] shadow-xl min-h-[340px] w-full card-item-div md1000:min-h-[260px]"
+          style={{
+            backgroundImage: 'url(https://c8.alamy.com/comp/PCYWYH/yoga-logo-or-label-fitness-meditation-symbol-vector-illustration-PCYWYH.jpg)',
+          }}
+          className="flex flex-col cursor-pointer bg-white justify-center py-6 px-10 text-center items-center mt-12 rounded-tl-[35px] rounded-br-[35px] shadow-xl min-h-[340px] w-full card-item-div md1000:min-h-[260px] "
         >
           <img src={card.img} alt="box_img" className=" w-[75px] mb-4" />
           <p className=" text-[24px] text-black font-bold uppercase mb-7">{card.title}</p>
@@ -17,6 +20,14 @@ function AboutCarts() {
           </p>
         </div>
       ))}
+
+
+
+
+
+
+
+
     </div>
     </>
   );
