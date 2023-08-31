e67d7bb421c366220cdd7144e1deda60a0c6bfa6
Mushfik Yeasir <pkm2243@gmail.com>
2023-08-31 22:48:50 +0600

    set loading and made card look better

--- client/src/pages/Instructors/Instructors.jsx
+++ client/src/pages/Instructors/Instructors.jsx
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
+import LoadingSpinner from '../../Components/Loader/LoadingSpinner'
 const Instructors = () => {
   const [instructors, setInstructors] = useState([])
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch(`http://localhost:5000/instructors`)
       .then((res) => res.json())
       .then((result) => {
         setInstructors(result);
+        setIsLoading(false);
       });
   }, []);
-  console.log(instructors);
+  
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>
+  }
+  // console.log(instructors);
   return (
     <div>
       <h1 className='text-center text-4xl text-cyan-700 border-b-4 border-indigo-500'>Our Beloved Instructors</h1>
-      <div className='grid gap-6 md:grid-cols-3 sm:grid-cols-2 mx-16 mt-5'>
+      <div className='grid gap-6 md:grid-cols-3 sm:grid-cols-2 mx-6 mt-5 my-10'>
         {
           instructors.map((sigleInstructors) =>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-              <figure><img src={sigleInstructors.image} alt="Person Image" /></figure>
+            <div className="card card-compact  bg-base-100 shadow-xl ">
+              <figure><img className='h-[250px] w-full ' src={sigleInstructors.image} alt="Person Image" /></figure>
               <div className="card-body">
-                <h2 className="card-title border-b">Name: {sigleInstructors.name}</h2>
-                <h2 className="card-title border-b"> Email: {sigleInstructors.email}</h2>
+                <h2 className="text-xl border-b">Name: {sigleInstructors.name}</h2>
+                <h2 className="text-xl  border-b"> Email: {sigleInstructors.email}</h2>
 
                 {/* <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
