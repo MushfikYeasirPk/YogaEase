00477f8f43ed3e25c3857e2c020d874b4ff28fbd
Mushfik Yeasir <pkm2243@gmail.com>
2023-09-01 14:20:09 +0600

    change signUP page button and svg

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -172,29 +172,27 @@ const Register = () => {
 
 
                         <div className="flex items-center justify-between">
-                            <button
-                                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                type="submit"
-                            >
-                                Sign Up
-                            </button>
+                        <Link to="" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span className="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Sign Up</span>
+                            </Link>
 
 
-                            <Link className="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-800" href="#">
+                            <Link className="inline-block align-baseline font-bold text-sm text-purple-700 hover:text-purple-800" href="#">
                                 Forgot Password?
                             </Link>
                         </div>
 
                     </form>
-                    <p className="text-center text-lg">
+                    <p className="text-center text-lg mb-5">
                         Already have an account?{' '}
-                        <Link to='/logIn' className="font-bold hover:underline" href="#">
+                        <Link to='/logIn' className="font-bold text-purple-700 hover:text-purple-800 hover:underline " href="#">
                             Sign in
                         </Link>
                     </p>
                 </div>
                 <div className=" h-[700px] w-[600px] lg:text-left p-10 ">
-                    <img className='lg:mt-20' src="sign-up.svg" alt="" />
+                    <img className='lg:mt-20' src="signInSignUp.svg" alt="" />
                     
                 </div>
             </div>
