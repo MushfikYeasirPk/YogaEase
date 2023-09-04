ffad104131e38f71f1dfcf19f87731f5959f939b
Mushfik Yeasir <pkm2243@gmail.com>
2023-09-04 12:58:51 +0600

    solves the repeating issue

--- client/src/Components/AnimatedTitle/AnimatedTitle.jsx
+++ client/src/Components/AnimatedTitle/AnimatedTitle.jsx
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
 function AnimatedTitle({ title }) {
-  const [displayedTitle, setDisplayedTitle] = useState('');
-
-  useEffect(() => {
-    let currentIndex = 0;
-    const titleLength = title.length;
-
-    const interval = setInterval(() => {
-      if (currentIndex < titleLength) {
-        setDisplayedTitle(prevTitle => prevTitle + title[currentIndex]);
-        currentIndex++;
-      } else {
-        clearInterval(interval);
+    const [displayedTitle, setDisplayedTitle] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    useEffect(() => {
+      if (currentIndex < title.length) {
+        const timeoutId = setTimeout(() => {
+          setDisplayedTitle(prevTitle => prevTitle + title[currentIndex]);
+          setCurrentIndex(prevIndex => prevIndex + 1);
+        }, 100); // You can adjust the delay between each letter here
+  
+        return () => clearTimeout(timeoutId);
       }
-    }, 100); // You can adjust the delay between each letter here
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [title]);
-
-  return (
-    <div>
-      <h1>{displayedTitle}</h1>
-    </div>
-  );
-}
+    }, [title, currentIndex]);
+  
+    return (
+      <div>
+        <h1 className='bg-slate-800 py-5 text-center text-4xl font-bold border-b-4 border-t-4 border-indigo-500 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>{displayedTitle}</h1>
+      </div>
+    );
+  }
 
 export default AnimatedTitle;
