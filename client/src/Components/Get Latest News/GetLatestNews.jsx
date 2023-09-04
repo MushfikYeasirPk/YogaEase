cc7a45cafe8cac66e27ca7f5d81d9a0715971d3b
Mushfik Yeasir <pkm2243@gmail.com>
2023-09-04 21:31:12 +0600

    get latest news compt

---
+++ client/src/Components/Get Latest News/GetLatestNews.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function GetLatestNews() {
+    return (
+        <>
+            <div className="lg:flex   items-center justify-center gap-5">
+                <div>
+                    <h2 className="text-2xl font-bold text-gray-900">
+                        Get the latest news!
+                    </h2>
+                </div>
+
+
+                    <div className="lg:flex gap-8">
+                        <div>
+                            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+                        </div>
+                        <div>
+                            <button type="submit" className="btn relative inline-flex items-center justify-start px-6 py-1 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span className="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Sign Up</span >
+                            </button>
+                        </div>
+
+                    </div>
+              </div>  
+
+        </>
+    )
+}
+
+export default GetLatestNews
