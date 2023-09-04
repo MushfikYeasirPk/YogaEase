ad57b77edbc8e1457f166651d319b1753d01a7cd
Mushfik Yeasir <pkm2243@gmail.com>
2023-09-04 20:56:25 +0600

    change text

--- client/src/Components/NeedYogaTrainer/NeedYogaTrainer.jsx
+++ client/src/Components/NeedYogaTrainer/NeedYogaTrainer.jsx
@@ -7,9 +7,9 @@ function NeedYogaTrainer() {
             <section className="bg-black w-full h-auto mb-10 "style={{ backgroundImage: 'url(https://cdn.pixabay.com/photo/2016/09/12/16/55/girl-1665173_960_720.jpg)' }}>
                 <div className=" text-white flex ml-24 py-10 flex-col justify-start items-start">
                     <div>
-                        <h2 className="text-[36px] font-bold">Need a Fitness Trainer?</h2>
+                        <h2 className="text-[36px] font-bold">Need a Yoga Trainer?</h2>
                         <h3 className="text-[28px] font-bold ">
-                            <span className="text-purple-700">Call:</span> +123-456789
+                            <span className="text-purple-700">Call:</span> +123456789
                         </h3>
                     </div>
                     
