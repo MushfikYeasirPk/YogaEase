ce62597f9661030930aef0349e61bf6951301c81
Mushfik Yeasir <pkm2243@gmail.com>
2023-09-05 16:18:44 +0600

    added aos fade-up

--- client/src/Components/PopularClasses/PopularClasses.jsx
+++ client/src/Components/PopularClasses/PopularClasses.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UseClasses from '../Custom Hooks/UseClasses';
 import './PopularClasses.css'
 import AnimatedTitle from '../AnimatedTitle/AnimatedTitle';
+import Aos from 'aos';
 const PopularClasses = () => {
     const [PopClasses] = UseClasses();
     // console.log("Pop class", PopClasses);
+    useEffect(()=>{
+        Aos.init()
+    })
     return (
         <div className='py-5'>
             <div className='bg-slate-900'>
@@ -12,7 +16,8 @@ const PopularClasses = () => {
             </div>
            
             
-            <div className='grid gap-6 md:grid-cols-3 sm:grid-cols-2 mx-16 mt-5'>
+            <div data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom" data-aos-duration="2000" className='grid gap-6 md:grid-cols-3 sm:grid-cols-2 mx-16 mt-5'>
                 {
                     PopClasses.slice(0, 6).map((SingleClass) =>
                        
